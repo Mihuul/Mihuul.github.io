c1bb15f0ddbb9fbd40a5908bf2055e7476f0bf3a
Mihuul <98897607+Mihuul@users.noreply.github.com>
2022-07-17 00:12:10 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-   const apiRoot = 'https://j-kodilla-tasks.herokuapp.com/v1/tasks';
+   const apiRoot = 'https://j-kodilla-tasks.herokuapp.com/v1/';
    const trelloApiRoot = 'https://j-kodilla-task.herokuapp.com/v1/trello';
    const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
    const $tasksContainer = $('[data-tasks-container]');
